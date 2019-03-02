7818c3835e5b457aa329896a6219d96ca8949abd
JoeNahm <joenahm@yeah.net>
2019-03-02 16:19:16 +0800

    完成JSON测试

--- 3-2-大三下/JSP/src/main/java/root/HelloServlet.java
+++ 3-2-大三下/JSP/src/main/java/root/HelloServlet.java
@@ -1,5 +1,7 @@
 package root;
 
+import org.json.JSONObject;
+
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
@@ -10,6 +12,15 @@ public class HelloServlet extends javax.servlet.http.HttpServlet {
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        response.getWriter().append("aa");
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("text/json;charset=utf-8");
+
+        JSONObject person = new JSONObject();
+
+        person.append("name", "张建");
+        person.append("gender", "男");
+        person.append("age", 20);
+
+        response.getWriter().append(person.toString());
     }
 }
