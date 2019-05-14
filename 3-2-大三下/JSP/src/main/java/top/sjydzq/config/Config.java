245723fdb05c3809af3927be5da35e7fad726dcb
JoeNahm <joenahm@yeah.net>
2019-05-14 11:19:01 +0800

    隐去信息

--- 3-2-大三下/JSP/src/main/java/top/sjydzq/config/Config.java
+++ 3-2-大三下/JSP/src/main/java/top/sjydzq/config/Config.java
@@ -2,9 +2,9 @@ package top.sjydzq.config;
 
 public abstract class Config {
     public final static String DB_DRIVER = "org.mariadb.jdbc.Driver";
-    public final static String DB_URL = "jdbc:mariadb://123.207.146.49:2614/itoffer";
-    public final static String DB_USERNAME = "joe";
-    public final static String DB_PASSWORD = "00001011";
+    public final static String DB_URL = "jdbc:mariadb://数据库地址";
+    public final static String DB_USERNAME = "用户名";
+    public final static String DB_PASSWORD = "密码";
     public final static int DEFAULT_PAGE_NO = 1;
     public final static int DEFAULT_PAGE_SIZE = 8;
 }
